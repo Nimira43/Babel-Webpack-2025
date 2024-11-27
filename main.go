3d520dfb2e2142a9981c79ca1de15770ce4510c9
Nimira43 <n.rabone72@aol.com>
2024-11-27 00:26:05 +0000

    Configuring router

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ import (
 
 func main() {
 	gRouter := mux.NewRouter()
+	gRouter.HandleFunc("/", HomeHandler)
 	http.ListenAndServe(":3000", gRouter)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "NimiraTech | Home Page")
 }
