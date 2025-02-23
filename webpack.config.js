c28d461f5c64af0923e9be958b9d4e8f1bc39050
Nimira43 <n.rabone72@aol.com>
2025-02-23 06:36:12 +0000

    Installed more Babel packages and updated webpack and babel configs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
+  mode: 'development', 
   entry: {
     app: [
       '@babel/polyfill',
@@ -19,7 +20,25 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-stage-0']
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-function-bind',
+              '@babel/plugin-proposal-export-default-from',
+              '@babel/plugin-proposal-logical-assignment-operators',
+              ['@babel/plugin-proposal-optional-chaining', { 'loose': false }],
+              ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
+              ['@babel/plugin-proposal-nullish-coalescing-operator', { 'loose': false }],
+              '@babel/plugin-proposal-do-expressions',
+              ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+              '@babel/plugin-proposal-function-sent',
+              '@babel/plugin-proposal-export-namespace-from',
+              '@babel/plugin-proposal-numeric-separator',
+              '@babel/plugin-proposal-throw-expressions',
+              '@babel/plugin-syntax-dynamic-import',
+              '@babel/plugin-syntax-import-meta',
+              ['@babel/plugin-proposal-class-properties', { 'loose': false }],
+              '@babel/plugin-proposal-json-strings'
+            ]
           }
         }
       }
@@ -28,6 +47,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public'), 
     compress: true,
-    port: 9000
+    port: 9000 
   }
-};
+}
+
